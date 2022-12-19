d756038eeeaddbee89258f811c39d4aa78e1d6fd
AlfonsoAD <alfonsoad097@gmail.com>
2022-12-18 20:23:19 -0600

    title

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title title="Divisas app" />
         <link rel="maifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
         <meta name="theme-color" content="#fff" />
